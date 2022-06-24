f5dd64d5cc1d78290b8e1509cb1b8d8695c8c721
Abdul_Wazed_Sobuz <abdulwazedsobuz@gmail.com>
2022-06-24 16:39:05 +0600

    Instractor Migration file added

---
+++ database/migrations/2022_06_24_102352_create_instractors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instractors', function (Blueprint $table) {
+            $table->id('instractor_id');
+            $table->string('instractor_name');
+            $table->string('email');
+            $table->string('gender');
+            $table->string('address')->nullable();
+            $table->integer('phone');
+            $table->unsignedBigInteger('account_id');
+            $table->foreign('account_id')->references('account_id')->on('accounts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instractors');
+    }
+};
