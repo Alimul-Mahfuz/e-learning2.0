73c35c20ffef7895c50fb59713e673730c5d0f31
Abdul_Wazed_Sobuz <abdulwazedsobuz@gmail.com>
2022-06-24 15:56:56 +0600

    Teacher and Qualification Migration file added

---
+++ database/migrations/2022_06_24_094722_create_teachers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id('teacher_id');
+            $table->string('teacher_name');
+            $table->string('email');
+            $table->string('gender');
+            $table->string('address')->nullable();
+            $table->unsignedBigInteger('qualification_id');
+            $table->unsignedBigInteger('account_id');
+            $table->string('profile_image')->nullable();
+            $table->foreign('qualification_id')->references('qualification_id')->on('qualifications');
+            $table->foreign('account_id')->references('account_id')->on('accounts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
