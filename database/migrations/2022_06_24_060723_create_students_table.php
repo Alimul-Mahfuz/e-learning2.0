2fb19fe623dba022418d2380507d8baab5b0a41f
Alimul Mahfuz Tushar <74812296+Alimul-Mahfuz@users.noreply.github.com>
2022-06-24 12:38:50 +0600

    Student migration file added

---
+++ database/migrations/2022_06_24_060723_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id('student_id');
+            $table->string('student_name');
+            $table->string('email');
+            $table->string('gender');
+            $table->string('address')->nullable();
+            $table->unsignedBigInteger('account_id');
+            $table->string('profile_image')->nullable();
+            $table->foreign('account_id')->references('account_id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
