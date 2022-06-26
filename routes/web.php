2d8d7894a909debc418ad0acb17e0956b19cfd0a
Alimul Mahfuz Tushar <74812296+Alimul-Mahfuz@users.noreply.github.com>
2022-06-26 20:30:43 +0600

    Conflict resolve

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,13 @@ Route::get('/', function () {
 Route::get('/login',[PagesController::class,'login'])->name('login');
 Route::post('/login',[AuthController::class,'login'])->name('login');
 Route::get('/student/register',[PagesController::class,'stdreg'])->name('stdreg');
+Route::get('/student/register',[PagesController::class,'stdreg'])->name('stdreg');
+Route::post('/student/register',[StudentAuthController::class,'studentreg'])->name('stdreg');
+Route::get('/student/dashboard',[PagesController::class,'studentdash'])->name('stddash');
+Route::get('/student/logout',[PagesController::class,'studentlogout'])->name('stdlogout');
+
+
+
 Route::get('/teacher/register',[teacherController::class,'teacherRegistration'])->name('teacherRegistration');
 Route::post('/teacher/register',[teacherController::class,'createSubmit'])->name('CreateSubmit');
 Route::get('/teacher/home',[teacherController::class,'teacherHome'])->name('teacherHome');
